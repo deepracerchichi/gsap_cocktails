65c70068f95f3a48035c9ed61c74575a708ad124
Chinyere Ukpong <reyeech@gmail.com>
2025-10-29 14:29:41 +0100

    Implemented better nav arrangement and finished cocktails menu section

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,9 @@ import React from 'react';
 import {ScrollTrigger, SplitText} from "gsap/all";
 import gsap from "gsap";
 import NavBar from './components/NavBar';
-import Hero from './components/Hero'
+import Hero from './components/Hero';
+import Cocktails from './components/Cocktails';
+
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
@@ -11,7 +13,8 @@ const App = () => {
         <main>
             <NavBar />
             <Hero />
-            <div className="h-dvh bg-black" />
+            <Cocktails />
+            
         </main>
     );
 };
